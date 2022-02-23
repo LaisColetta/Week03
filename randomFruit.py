04d4dbfed89346d703c8f937434579d3c35656dc
LaisColetta <99751201+LaisColetta@users.noreply.github.com>
2022-02-23 19:12:30 +0000

    Delete randomFruit.py

--- randomFruit.py
+++
@@ -1,9 +0,0 @@
-#Program that prints out a random fruit
-#Author: Lais
-
-import random
-fruits = ['apple', 'banana', 'cherry']
-index = random.randint(0, len(fruits)-1)
-
-fruit = fruits[index]
-print("A Random Fruit:{} ".format(fruit))
