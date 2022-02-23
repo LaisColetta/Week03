84618597c11be8949e09d4197e8abf2bc5fe754e
LaisColetta <99751201+LaisColetta@users.noreply.github.com>
2022-02-23 19:13:04 +0000

    Delete sub.py

--- sub.py
+++
@@ -1,8 +0,0 @@
-#Program that subtract one number from another
-#Author: Lais
-
-
-num1 = input ('Enter first number: ')
-num2 = input ('Enter secind number: ')
-result =  num1 - num2
-print ('{} minus {} is {}'.format(num1, num2, result))
