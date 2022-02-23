60653443880226ead42ef814b5ca0a637eec5188
LaisColetta <99751201+LaisColetta@users.noreply.github.com>
2022-02-23 18:53:34 +0000

    Add files via upload

---
+++ absolute.py
@@ -0,0 +1,7 @@
+#Program that takes a number and give its absolute value
+#Author: Lais
+
+number = float(input("Enter a number:"))
+
+absoluteValue = abs(number)
+print('The absolute value of {} is {}'.format(number, absoluteValue))
---
+++ sub.py
@@ -0,0 +1,8 @@
+#Program that subtract one number from another
+#Author: Lais
+
+
+num1 = input ('Enter first number: ')
+num2 = input ('Enter secind number: ')
+result =  num1 - num2
+print ('{} minus {} is {}'.format(num1, num2, result))
