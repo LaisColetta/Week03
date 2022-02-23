4af80c5419aa4c59cc045ba75fb49fc0df017fab
LaisColetta <99751201+LaisColetta@users.noreply.github.com>
2022-02-23 19:11:13 +0000

    Delete absolute.py

--- absolute.py
+++
@@ -1,7 +0,0 @@
-#Program that takes a number and give its absolute value
-#Author: Lais
-
-number = float(input("Enter a number:"))
-
-absoluteValue = abs(number)
-print('The absolute value of {} is {}'.format(number, absoluteValue))
