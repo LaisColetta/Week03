4e4e6f724b2e5ac7faa9574e2cbe151aad9450d8
LaisColetta <99751201+LaisColetta@users.noreply.github.com>
2022-02-23 19:12:59 +0000

    Delete randomFruit2.py

--- randomFruit2.py
+++
@@ -1,9 +0,0 @@
-#Modify the program in 6 (randomFruit2.py) so that it uses a tuple ( ) not a list [ ]. 
-#Author: Lais
-
-import random
-fruits = ('Apple', 'Orange', 'Banana', 'Pear')
-index = random.randint (0, len(fruits)-1)
-
-fruit = fruits [index]
-print ("A random fruit: {} ".format(fruit))
